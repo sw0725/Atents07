5e6c449c64d664c66616346993edbd47a3814d73
test <120767913+sw0725@users.noreply.github.com>
2023-12-26 15:22:58 +0900

    12/26_1

---
+++ 저장용/01_Console/01_Console/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("이곳에 당신의 이름을 적어주십시오 : ");
+
+            string name = Console.ReadLine();
+            for (int i = 0; i < 3; i++) 
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
