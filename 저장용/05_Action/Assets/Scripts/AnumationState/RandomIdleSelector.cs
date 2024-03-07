220aae2bb3419dcbc10b484f5af67a9813ee08b3
yeolchiyang <yangchi97@naver.com>
2024-03-07 14:34:43 +0900

    03/07

---
+++ 저장용/05_Action/Assets/Scripts/AnumationState/RandomIdleSelector.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class RandomIdleSelector : StateMachineBehaviour
+{
+    readonly int IdleSelect_Hash = Animator.StringToHash("IdleSelect");
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetInteger(IdleSelect_Hash, RandomSelect());
+    }
+
+    int RandomSelect() 
+    {
+        int select = 0;
+        float num = Random.value;
+
+        if (num < 0.05f)        //5ÆÛ
+        {
+            select = 4;
+        }
+        else if (num < 0.10f) 
+        {
+            select = 3;
+        }
+        else if (num < 0.15f)
+        {
+            select = 2;
+        }
+        else if (num < 0.20f)
+        {
+            select = 1;
+        }
+        else
+        {
+            select = 0;
+        }
+        return select;
+    }
+}
---
+++ 저장용/05_Action/Assets/Scripts/Player/PlayerInputController.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+{
+    public float walkSpeed = 3.0f;
+    public float runSpeed = 5.0f;
+    
+    float currentSpeed = 0.0f;
+
+    enum MoveMode 
+    {
+        Walk = 0,
+        Run
+    }
+    MoveMode currentSpeedMode = MoveMode.Run;
+    MoveMode CurrentSpeedMode 
+    {
+        get => currentSpeedMode;
+        set 
+        {
+            currentSpeedMode = value;
+            if (currentSpeed > 0.0f) 
+            {
+                MoveSpeedChange(currentSpeedMode);
+            }
+        }
+    }
+    Vector3 inputDir = Vector3.zero;    //점프없음 - y=0
+
+    PlayerInputAction inputAction;
+    Animator animator;
+    CharacterController characterController;
+
+    readonly int speed_Hash = Animator.StringToHash("Speed");
+    const float AnimatorStopSpeed = 0.0f;
+    const float AnimatorWalkSpeed = 0.3f;
+    const float AnimatorRunSpeed = 1.0f;
+
+    private void Awake()
+    {
+        inputAction = new PlayerInputAction();
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void OnEnable()
+    {
+        inputAction.Player.Enable();
+        inputAction.Player.Move.performed += OnMove;
+        inputAction.Player.Move.canceled += OnMove;
+        inputAction.Player.MoveModeChange.performed += OnMoveModeChage;
+    }
+
+    private void OnDisable()
+    {
+        inputAction.Player.Move.performed -= OnMove;
+        inputAction.Player.Move.canceled -= OnMove;
+        inputAction.Player.MoveModeChange.performed -= OnMoveModeChage;
+        inputAction.Player.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        Vector3 input = context.ReadValue<Vector2>();
+
+        inputDir.x = input.x;
+        inputDir.y = 0;
+        inputDir.z = input.y;
+        if (!context.canceled) //눌려짐
+        {
+            MoveSpeedChange(currentSpeedMode);
+        }
+        else 
+        {
+            currentSpeed = 0;
+            animator.SetFloat(speed_Hash, AnimatorStopSpeed);
+        }
+    }
+
+    void MoveSpeedChange(MoveMode mode) 
+    {
+        switch (mode)
+        {
+            case MoveMode.Walk:
+                currentSpeed = walkSpeed;
+                animator.SetFloat(speed_Hash, AnimatorWalkSpeed);
+                break;
+            case MoveMode.Run:
+                currentSpeed = runSpeed;
+                animator.SetFloat(speed_Hash, AnimatorRunSpeed);
+                break;
+        }
+    }
+
+    private void OnMoveModeChage(InputAction.CallbackContext context)
+    {
+        if (CurrentSpeedMode == MoveMode.Walk)
+        {
+            CurrentSpeedMode = MoveMode.Run;
+        }
+        else 
+        {
+            CurrentSpeedMode -= MoveMode.Walk;
+        }
+    }
+}
